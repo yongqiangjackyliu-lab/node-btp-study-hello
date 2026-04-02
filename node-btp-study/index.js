d97688c0660eae1fe07b11860248e022c9ecac6b
YONGQIANG LIU <yongqiang.jacky.liu@gmail.com>
2026-04-02 05:00:35 +0000

    Update BAS project files

--- node-btp-study/index.js
+++ node-btp-study/index.js
@@ -1,23 +1,12 @@
-//console.log("Hello Node Study!");
-/*
-const http = require("http");
+const express = require('express');
+var app = express();
+var defaultport = 3003;
+var port = process.env.PORT || defaultport;
 
-const server = http.createServer((req, res) => {
-  res.end("Hello BTP Ready Node");
+app.get('/', function(req, res){
+    res.send(process.env.HELLO || "Default");
 });
 
-server.listen(3002);
-*/
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  console.log("Request received");
-  res.end("Hello BTP Ready Node");
+app.listen(port, function(){
+    console.log("APP listens on port:" + port);
 });
-
-const port = process.env.PORT || 3002;
-server.listen(port, () => {
-  console.log("Server started on port " + port);
-});
-
-var test = 10;
